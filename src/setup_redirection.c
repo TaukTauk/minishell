8aa85fe11fd93591ce10453f3dc8eacca3c5f820
Tauk Tauk Lin <talin@z2t13c3.42bangkok.com>
2025-03-03 16:56:19 +0700

    update

--- src/setup_redirection.c
+++ src/setup_redirection.c
@@ -6,7 +6,7 @@
 /*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 09:44:06 by rick              #+#    #+#             */
-/*   Updated: 2025/03/02 15:44:12 by talin            ###   ########.fr       */
+/*   Updated: 2025/03/03 16:54:33 by talin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ int	setup_delimeter(t_command *command, t_data *data)
 	pid = fork();
 	if (pid == -1)
 		return (close_err(fd[0], fd[1]), 1);
-	if (pid == 0) {
+	if (pid == 0)
 		write_pipe(fd, command);
-	}
 	else
 	{
 		close(fd[1]);
@@ -39,6 +38,16 @@ int	setup_delimeter(t_command *command, t_data *data)
 		update_exit_status(pid, fd, data);
 		return (0);
 	}
+	return (0);
+}
+
+int	ft_input_access(t_io_file *input, t_data *data, int *fd)
+{
+	if (access(input->file_name, F_OK) != 0)
+		return (print_err_nofile(input->file_name, data), 1);
+	*fd = open(input->file_name, O_RDONLY);
+	if (*fd == -1)
+		return (print_err_nofile(input->file_name, data), 1);
 	return (0);
 }
 
@@ -56,11 +65,8 @@ int	setup_input_redirection(t_command *command, t_data *data)
 		{
 			if (input)
 			{
-				if (access(input->file_name, F_OK) != 0)
-					return (print_err_nofile(input->file_name, data), 1);
-				fd = open(input->file_name, O_RDONLY);
-				if (fd == -1)
-					return (print_err_nofile(input->file_name, data), 1);
+				if (ft_input_access(input, data, &fd))
+					return (1);
 				if (command->fd_in != -1)
 					close(command->fd_in);
 				command->fd_in = fd;
@@ -74,12 +80,83 @@ int	setup_input_redirection(t_command *command, t_data *data)
 	return (1);
 }
 
-int	setup_output_redirection(t_data *data, t_command *command)
+int	outfileappend_setup(t_command **command, t_data *data, int *status)
+{
+	int			flags;
+	t_io_file	*current;
+	int			fd;
+
+	flags = O_WRONLY | O_CREAT | O_APPEND;
+	current = (*command)->outfileappend;
+	while (current)
+	{
+		fd = open(current->file_name, flags, 0644);
+		if (fd == -1)
+			return (print_err_nofile(current->file_name, data), 1);
+		if (current->order_value == (*command)->output_order && !status)
+		{
+			printf("hello\n");
+			*status = 1;
+			(*command)->fd_out = fd;
+		}
+		else
+			close(fd);
+		current = current->next;
+	}
+	return (0);
+}
+
+int	outfile_setup(t_command **command, t_data *data, int *status)
+{
+	int			flags;
+	t_io_file	*current;
+	int			fd;
+
+	flags = O_WRONLY | O_CREAT | O_TRUNC;
+	current = (*command)->outfile;
+	while (current)
+	{
+		fd = open(current->file_name, flags, 0644);
+		if (fd == -1)
+			return (print_err_nofile(current->file_name, data), 1);
+		if (current->order_value == (*command)->output_order && !status)
+		{
+			printf("hi\n");
+			*status = 1;
+			(*command)->fd_out = fd;
+		}
+		else
+			close(fd);
+		current = current->next;
+	}
+	return (0);
+}
+
+int	setup_output_red(t_command **command, t_data *data)
 {
 	int			fd;
 	int			flags;
 	t_io_file	*last_output;
-	t_io_file	*current;
+
+	if ((*command)->outfileappend)
+	{
+		last_output = (*command)->outfileappend;
+		flags = O_WRONLY | O_CREAT | O_APPEND;
+	}
+	else
+	{
+		last_output = (*command)->outfile;
+		flags = O_WRONLY | O_CREAT | O_TRUNC;
+	}
+	fd = open(last_output->file_name, flags, 0644);
+	if (fd == -1)
+		return (print_err_nofile(last_output->file_name, data), 1);
+	(*command)->fd_out = fd;
+	return (0);
+}
+
+int	setup_output_redirection(t_data *data, t_command *command)
+{
 	int			status;
 
 	if (!command->outfile && !command->outfileappend)
@@ -87,62 +164,20 @@ int	setup_output_redirection(t_data *data, t_command *command)
 	status = 0;
 	if (command->outfileappend)
 	{
-		flags = O_WRONLY | O_CREAT | O_APPEND;
-		current = command->outfileappend;
-		while (current)
-		{
-			fd = open(current->file_name, flags, 0644);
-			if (fd == -1)
-				return (print_err_nofile(current->file_name, data), 1);
-			if (current->order_value == command->output_order)
-			{
-				last_output = current;
-				status = 1;
-				break ;
-			}
-			else
-				close(fd);
-			current = current->next;
-		}
+		if (outfileappend_setup(&command, data, &status))
+			return (1);
 	}
-	if (command->outfile && !status)
+	if (command->outfile)
 	{
-		flags = O_WRONLY | O_CREAT | O_TRUNC;
-		current = command->outfile;
-		while (current)
-		{
-			fd = open(current->file_name, flags, 0644);
-			if (fd == -1)
-				return (print_err_nofile(current->file_name, data), 1);
-			if (current->order_value == command->output_order)
-			{
-				last_output = current;
-				status = 1;
-				break ;
-			}
-			else
-				close(fd);
-			current = current->next;
-		}
+		if (outfile_setup(&command, data, &status))
+			return (1);
 	}
 	if (!status)
 	{
-		if (command->outfileappend)
-		{
-			last_output = command->outfileappend;
-			flags = O_WRONLY | O_CREAT | O_APPEND;
-		}
-		else
-		{
-			last_output = command->outfile;
-			flags = O_WRONLY | O_CREAT | O_TRUNC;
-		}
-		fd = open(last_output->file_name, flags, 0644);
-		if (fd == -1)
-			return (print_err_nofile(last_output->file_name, data), 1);
+		if (setup_output_red(&command, data))
+			return (1);
 	}
-	command->fd_out = fd;
-	if (dup2(fd, STDOUT_FILENO) == -1)
-		return (close(fd), 1);
+	if (dup2(command->fd_out, STDOUT_FILENO) == -1)
+		return (close(command->fd_out), 1);
 	return (0);
 }
