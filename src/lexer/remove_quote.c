3be811a034238a888dad0885faa6269346cfd3e6
Tauk Tauk Lin <talin@z3t5c1.42bangkok.com>
2025-03-19 14:58:12 +0700

    quote

--- src/lexer/remove_quote.c
+++ src/lexer/remove_quote.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   remove_quote.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juhtoo-h <juhtoo-h@student.42.fr>          +#+  +:+       +#+        */
+/*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 10:39:10 by talin             #+#    #+#             */
-/*   Updated: 2025/03/05 13:52:32 by juhtoo-h         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:55:47 by talin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,36 @@ static int	size_of_str_without_quote(char **str)
 		{
 			in_quote = 0;
 			quote_char = 0;
+			size -= 2;
 		}
-		else
-			size++;
+		size++;
 	}
 	return (size);
+}
+
+static int	in_quote_or_not(char *str)
+{
+	int	in_quote;
+	int	quote_char;
+	int	i;
+
+	i = -1;
+	in_quote = 0;
+	quote_char = 0;
+	while (str[++i])
+	{
+		if ((str[i] == '"' || str[i] == '\'') && !in_quote)
+		{
+			in_quote = 1;
+			quote_char = str[i];
+		}
+		else if (str[i] == quote_char && in_quote)
+		{
+			in_quote = 0;
+			quote_char = 0;
+		}
+	}
+	return (in_quote);
 }
 
 static void	copy_str_without_quote(char **clean_str, int in_quote,
@@ -49,6 +74,11 @@ static void	copy_str_without_quote(char **clean_str, int in_quote,
 
 	i = -1;
 	j = 0;
+	if (in_quote_or_not(*str))
+	{
+		ft_strcpy(*clean_str, *str);
+		return ;
+	}
 	while ((*str)[++i])
 	{
 		if (((*str)[i] == '"' || (*str)[i] == '\'') && !in_quote)
